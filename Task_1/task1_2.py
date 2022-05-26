1750334f9aec449bce870aaa0a1a6f09a1bfb820
AndreiKazakov1 <ankazakov1.21@gmail.com>
2022-05-26 17:14:01 +0300

    tsk_2

--- Task_1/task1_2.py
+++ Task_1/task1_2.py
@@ -3,3 +3,12 @@
 # Найдите минимальный по модулю элемент списка.
 
 
+from random import randrange
+
+# список со случ числами от -10 до 50
+numlist = [randrange(-10, 20) for i in range(int(input("введите размер списка чисел :")))]
+print(f'исходный список {numlist}')
+
+list_1 = list(map(lambda x: x if x % 2 != 0 else -12, numlist))
+print(list_1)
+print(max(list_1))
