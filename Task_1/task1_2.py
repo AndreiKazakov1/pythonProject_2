ced7892ce492cca94f99e1566b6a10eed0b90c21
AndreiKazakov1 <ankazakov1.21@gmail.com>
2022-06-01 08:31:20 +0300

    tsk_2 ch

--- Task_1/task1_2.py
+++ Task_1/task1_2.py
@@ -2,13 +2,16 @@
 # Введите одномерный целочисленный список. Найдите наибольший нечетный элемент.
 # Найдите минимальный по модулю элемент списка.
 
-
 from random import randrange
 
 # список со случ числами от -10 до 50
-numlist = [randrange(-10, 20) for i in range(int(input("введите размер списка чисел :")))]
+numlist = [randrange(-20, 20) for i in range(int(input("введите размер списка чисел :")))]
 print(f'исходный список {numlist}')
 
-list_1 = list(map(lambda x: x if x % 2 != 0 else -12, numlist))
+list_1 = list(map(lambda x: x if x % 2 != 0 else False, numlist))
 print(list_1)
-print(max(list_1))
+print(f'наибольший нечетный элемент {max(list_1)}')
+
+list_2 = list(map(lambda x: abs(x), numlist))
+print(list_2)
+print(f'минимальный по модулю элемент списка {min(list_2)}')
